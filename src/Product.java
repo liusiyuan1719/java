c65335776cb469ac1029773aad11464a0068d156
lenovo <3248693159.@qq.com>
2020-12-29 21:08:17 +0800

    1.完成27号

---
+++ src/Product.java
@@ -0,0 +1,40 @@
+public class Product {
+    private String id;
+    private String productname;
+    private String price;
+    private String desc   ;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+
+}
