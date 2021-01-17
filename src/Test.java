12faf24699cb42fc09bfa51ced22186e8f25f86d
liusiyuan1719 <3248693159@qq.com>
2021-01-17 23:54:00 +0800

    1.优化代码

--- src/Test.java
+++ src/Test.java
@@ -28,54 +28,43 @@ public class Test {
                     System.out.println("登陆成功");
                     Product carts[]=new Product[20];
                     while (bo1){
-                        inPro=null;
-                        inPro=Class.forName("Test").getResourceAsStream("/product.xlsx");
-                        ReadProductExcel readProductExcel=new ReadProductExcel();
-                        Product products[] =readProductExcel.getAllProductExcel(inPro);
-                        for (Product product:products) {
-                            System.out.print(product.getId());
-                            System.out.print("\t" + product.getProductname());
-                            System.out.print("\t" + product.getPrice());
-                            System.out.println("\t" + product.getDesc());
-                        }
-                        System.out.println("请输入商品ID");
-                        String pId=sc.next();
-                        inPro=null;
-                        inPro=Class.forName("Test").getResourceAsStream("/product.xlsx");
-                        ReadProductExcel readProductExcel1=new ReadProductExcel();
-                        Product product=readProductExcel1.getProductById(pId,inPro);
-                        if(product!=null) {
-                            count++;
-                            carts[count] = product;
-                            System.out.println("找到了该商品");
-                            System.out.print(product.getId());
-                            System.out.print("\t" + product.getProductname());
-                            System.out.print("\t" + product.getPrice());
-                            System.out.println("\t" + product.getDesc());
-
-                        }
-                        else {
-                            System.out.println("没有该商品");
-                        }
-                        System.out.println("是否继续购买商品，请输入yes or no");
-                        String yn=sc.next();
-                        if (yn.equals("yes"))
-                        {
-                            /*
+                        System.out.println("购买商品请输入1");
+                        System.out.println("查看购物车请输入2");
+                        System.out.println("结账请输入3");
+                        System.out.println("退出请输入4");
+                        int yn1=sc.nextInt();
+                        if (yn1==1){
                             inPro=null;
                             inPro=Class.forName("Test").getResourceAsStream("/product.xlsx");
-                            readProductExcel=new ReadProductExcel();
-                            products =readProductExcel.getAllProductExcel(inPro);
-                            for (Product product1:products) {
-                                System.out.print(product1.getId());
-                                System.out.print("\t" + product1.getProductname());
-                                System.out.print("\t" + product1.getPrice());
-                                System.out.println("\t" + product1.getDesc());
+                            ReadProductExcel readProductExcel=new ReadProductExcel();
+                            Product products[] =readProductExcel.getAllProductExcel(inPro);
+                            for (Product product:products) {
+                                System.out.print(product.getId());
+                                System.out.print("\t" + product.getProductname());
+                                System.out.print("\t" + product.getPrice());
+                                System.out.println("\t" + product.getDesc());
                             }
-                            */
-                            bo1=true;
+                            System.out.println("请输入商品ID");
+                            String pId=sc.next();
+                            inPro=null;
+                            inPro=Class.forName("Test").getResourceAsStream("/product.xlsx");
+                            ReadProductExcel readProductExcel1=new ReadProductExcel();
+                            Product product=readProductExcel1.getProductById(pId,inPro);
+                            if(product!=null) {
+                                count++;
+                                carts[count] = product;
+                                System.out.println("找到了该商品");
+                                System.out.print(product.getId());
+                                System.out.print("\t" + product.getProductname());
+                                System.out.print("\t" + product.getPrice());
+                                System.out.println("\t" + product.getDesc());
+
+                            }
+                            else {
+                                System.out.println("没有该商品");
+                            }
                         }
-                        else {
+                        else if (yn1==2){
                             System.out.println("你购买的商品数量为" + count);
                             for (int j=0;j<=count;j++){
                                 if (carts[j]!=null) {
@@ -87,25 +76,13 @@ public class Test {
                                     money = money + num;
                                 }
                             }
-                            /*
-                            for (int j=0;j<carts.length;j++){
-                                if (carts[j]!=null) {
-                                    System.out.print(carts[j].getId());
-                                    System.out.print("\t" + carts[j].getProductname());
-                                    System.out.print("\t" + carts[j].getPrice());
-                                    System.out.println("\t" + carts[j].getDesc());
-                                    int num=Integer.parseInt(carts[j].getPrice());
-                                    money=money+num;
-                                }
-                            }
-                            */
+                        }
+                        else if (yn1==3){
+                            System.out.println("您需要付" + money + "元");
+                        }
+                        else if (yn1==4){
                             bo1=false;
                         }
-                    }
-                    System.out.println("立即付款请输入 1 ，售后付款请输入 0");
-                    int yn2=sc.nextInt();
-                    if (yn2==1) {
-                        System.out.println("您需要付" + money + "元");
                     }
                     bo=false;
                     break;
