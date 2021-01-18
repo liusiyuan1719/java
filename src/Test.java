d28bcb76e95a650a5c8fd69b0c92c1815410b767
liusiyuan1719 <3248693159@qq.com>
2021-01-18 20:55:51 +0800

    1.解决只能输入第一位用户密码的问题

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,9 @@
 import java.io.InputStream;
+import java.util.Random;
 import java.util.Scanner;
 import java.io.File;
 
 public class Test {
-
     public static void main(String[] args) throws ClassNotFoundException {
         boolean bo=true;
         boolean bo1=true;
@@ -18,7 +18,6 @@ public class Test {
             System.out.println("请输入你的密码:");
             String password=sc.next();
 
-            InputStream inPro=Class.forName("Test").getResourceAsStream("product.xlsx");
             InputStream in = Class.forName("Test").getResourceAsStream("/users.xlsx");
             //File file=new File("C:\\Users\\151509\\IdeaProjects\\java\\src\\users.xlsx");
             ReadUsersExcel readExcel=new ReadUsersExcel();
@@ -26,72 +25,76 @@ public class Test {
             for (int i=0; i<users.length;i++) {
                 if (uername.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
                     System.out.println("登陆成功");
-                    Product carts[]=new Product[20];
-                    while (bo1){
-                        System.out.println("购买商品请输入1");
-                        System.out.println("查看购物车请输入2");
-                        System.out.println("结账请输入3");
-                        System.out.println("退出请输入4");
-                        int yn1=sc.nextInt();
-                        if (yn1==1){
-                            inPro=null;
-                            inPro=Class.forName("Test").getResourceAsStream("/product.xlsx");
-                            ReadProductExcel readProductExcel=new ReadProductExcel();
-                            Product products[] =readProductExcel.getAllProductExcel(inPro);
-                            for (Product product:products) {
-                                System.out.print(product.getId());
-                                System.out.print("\t" + product.getProductname());
-                                System.out.print("\t" + product.getPrice());
-                                System.out.println("\t" + product.getDesc());
-                            }
-                            System.out.println("请输入商品ID");
-                            String pId=sc.next();
-                            inPro=null;
-                            inPro=Class.forName("Test").getResourceAsStream("/product.xlsx");
-                            ReadProductExcel readProductExcel1=new ReadProductExcel();
-                            Product product=readProductExcel1.getProductById(pId,inPro);
-                            if(product!=null) {
-                                count++;
-                                carts[count] = product;
-                                System.out.println("找到了该商品");
-                                System.out.print(product.getId());
-                                System.out.print("\t" + product.getProductname());
-                                System.out.print("\t" + product.getPrice());
-                                System.out.println("\t" + product.getDesc());
-
-                            }
-                            else {
-                                System.out.println("没有该商品");
-                            }
-                        }
-                        else if (yn1==2){
-                            System.out.println("你购买的商品数量为" + count);
-                            for (int j=0;j<=count;j++){
-                                if (carts[j]!=null) {
-                                    System.out.print(carts[j].getId());
-                                    System.out.print("\t" + carts[j].getProductname());
-                                    System.out.print("\t" + carts[j].getPrice());
-                                    System.out.println("\t" + carts[j].getDesc());
-                                    int num = Integer.parseInt(carts[j].getPrice());
-                                    money = money + num;
-                                }
-                            }
-                        }
-                        else if (yn1==3){
-                            System.out.println("您需要付" + money + "元");
-                        }
-                        else if (yn1==4){
-                            bo1=false;
-                        }
-                    }
                     bo=false;
                     break;
                 }
-                else {
+                else if(i==(users.length-1)){
                     System.out.println("登陆失败");
+                /*    System.out.println("正确的用户名是" + users[i].getUsername());
                     System.out.println("正确的密码是" + users[i].getPassword());
-                    break;
+                    //测试密码
+                    */
                 }
+            }
+        }
+        Scanner sc=new Scanner(System.in);//输入输出流
+        InputStream inPro=Class.forName("Test").getResourceAsStream("product.xlsx");
+        Product carts[]=new Product[20];
+        while (bo1){
+            System.out.println("购买商品请输入1");
+            System.out.println("查看购物车请输入2");
+            System.out.println("结账请输入3");
+            System.out.println("退出请输入4");
+            int yn1=sc.nextInt();
+            if (yn1==1){
+                inPro=null;
+                inPro=Class.forName("Test").getResourceAsStream("/product.xlsx");
+                ReadProductExcel readProductExcel=new ReadProductExcel();
+                Product products[] =readProductExcel.getAllProductExcel(inPro);
+                for (Product product:products) {
+                    System.out.print(product.getId());
+                    System.out.print("\t" + product.getProductname());
+                    System.out.print("\t" + product.getPrice());
+                    System.out.println("\t" + product.getDesc());
+                }
+                System.out.println("请输入商品ID");
+                String pId=sc.next();
+                inPro=null;
+                inPro=Class.forName("Test").getResourceAsStream("/product.xlsx");
+                ReadProductExcel readProductExcel1=new ReadProductExcel();
+                Product product=readProductExcel1.getProductById(pId,inPro);
+                if(product!=null) {
+                    count++;
+                    carts[count] = product;
+                    System.out.println("找到了该商品");
+                    System.out.print(product.getId());
+                    System.out.print("\t" + product.getProductname());
+                    System.out.print("\t" + product.getPrice());
+                    System.out.println("\t" + product.getDesc());
+
+                }
+                else {
+                    System.out.println("没有该商品");
+                }
+            }
+            else if (yn1==2){
+                System.out.println("你购买的商品数量为" + count);
+                for (int j=0;j<=count;j++){
+                    if (carts[j]!=null) {
+                        System.out.print(carts[j].getId());
+                        System.out.print("\t" + carts[j].getProductname());
+                        System.out.print("\t" + carts[j].getPrice());
+                        System.out.println("\t" + carts[j].getDesc());
+                        int num = Integer.parseInt(carts[j].getPrice());
+                        money = money + num;
+                    }
+                }
+            }
+            else if (yn1==3){
+                System.out.println("您需要付" + money + "元");
+            }
+            else if (yn1==4){
+                bo1=false;
             }
         }
     }
